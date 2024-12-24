bd3b3a67578376a6e2aa4d0fb69c8f9ef42ba893
FC (Fay) Stegerman <flx@obfusk.net>
2024-12-24 22:14:35 +0100

    tests: use minSdk 23/24

--- test/test-compare-in.sh
+++ test/test-compare-in.sh
@@ -4,28 +4,30 @@ export LC_ALL=C.UTF-8
 for apk in apks/apks/golden-aligned-*out.apk; do
   echo "$apk"
   if unzip -l "$apk" 2>/dev/null | grep -qF META-INF/MANIFEST.MF; then
-    min=
+    min=23
   else
-    min=--min-sdk-version=24
+    min=24
   fi
-  apksigcopier compare $min "$apk" --unsigned apks/apks/golden-aligned-in.apk
+  apksigcopier compare --min-sdk-version=$min "$apk" --unsigned \
+    apks/apks/golden-aligned-in.apk
 done
 for apk in apks/apks/golden-legacy-aligned-*out.apk; do
   echo "$apk"
   if unzip -l "$apk" 2>/dev/null | grep -qF META-INF/MANIFEST.MF; then
-    min=
+    min=23
   else
-    min=--min-sdk-version=24
+    min=24
   fi
-  apksigcopier compare $min "$apk" --unsigned apks/apks/golden-legacy-aligned-in.apk
+  apksigcopier compare --min-sdk-version=$min "$apk" --unsigned \
+    apks/apks/golden-legacy-aligned-in.apk
 done
 for apk in apks/apks/golden-unaligned-*out.apk; do
   echo "$apk"
   if unzip -l "$apk" 2>/dev/null | grep -qF META-INF/MANIFEST.MF; then
-    min=
+    min=23
   else
-    min=--min-sdk-version=24
+    min=24
   fi
-  APKSIGCOPIER_SKIP_REALIGNMENT=1 apksigcopier compare $min "$apk" \
+  APKSIGCOPIER_SKIP_REALIGNMENT=1 apksigcopier compare --min-sdk-version=$min "$apk" \
     --unsigned apks/apks/golden-unaligned-in.apk
 done
